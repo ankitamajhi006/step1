4244377a3a067a2870f5e313d3fd4f74a7fd2c05
ankitamajhi006 <ankitamajhi551@gmail.com>
2026-03-05 15:07:28 +0530

    case 7

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -136,3 +136,34 @@ import java.util.Stack;
             }
         }
     }
+    import java.util.Deque;
+import java.util.LinkedList;
+
+    public class UseCase7PalindromeCheckerApp {
+        public static void main(String[] args) {
+            // Word to check
+            String word = "rotor";
+
+            // Create a deque and add all characters
+            Deque<Character> deque = new LinkedList<>();
+            for (char ch : word.toCharArray()) {
+                deque.addLast(ch);
+            }
+
+            // Check palindrome by comparing front and rear
+            boolean isPalindrome = true;
+            while (deque.size() > 1) {
+                if (!deque.removeFirst().equals(deque.removeLast())) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            // Print result
+            if (isPalindrome) {
+                System.out.println("The word \"" + word + "\" is a palindrome using Deque.");
+            } else {
+                System.out.println("The word \"" + word + "\" is NOT a palindrome using Deque.");
+            }
+        }
+    }
